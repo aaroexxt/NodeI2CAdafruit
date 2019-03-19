cef7ab812bb6b8c6226205e9fc0794ba092d7e63
aaroexxt <aaron.becker@gmail.com>
2019-03-18 23:10:40 -0700

    basic fakeGPIO setup

--- Si4713/controller.js
+++ Si4713/controller.js
@@ -9,12 +9,14 @@ console.log("requiring common lib");
 const LibCommon = require("../common.js");
 
 console.log("requiring fakeGPIO lib");
+const GPIO = require("./fakeGPIO.js").Gpio;
 
 class Si4713Driver extends LibCommon.device {
 	constructor(pin = -1) {
 		super(("Si4713#"+Math.random().toFixed(3)*1000), 1); //call super to set parameters
 
 		this.resetpin = pin;
+		this.resetGPIO = new GPIO(this.resetpin, "out"); //create the pin
 	}
 	begin(addr = lC.SI4710_ADDR1) {
 		console.log("begin called w/addr "+addr);
@@ -24,6 +26,17 @@ class Si4713Driver extends LibCommon.device {
 		this.powerUp();
 	}
 	reset() {
+		if (this.resetpin > 0) { //ensure that pin was created correctly
+			this.resetGPIO.writeSync(1);
+			setTimeout( () => {
+				this.resetGPIO.writeSync(0);
+				setTimeout( () => {
+					this.resetGPIO.writeSync(1);
+				}, 10);
+			},10);
+		}
+	}
+	powerUp() {
 
 	}
 }
---
+++ Si4713/fakeGPIO.js
@@ -0,0 +1,15 @@
+//FakeGPIO.js
+//Emulated onoff module from npm so I can develop on my mac
+https://github.com/fivdi/onoff
+
+
+module.exports = {
+	Gpio: function(pin, mode) {
+		console.log("new Gpio created; pin="+pin+", mode="+mode);
+		this.pin = pin;
+		this.mode = mode;
+		this.writeSync = function(pwr) {
+			console.log("writing to GPIO at pin "+this.pin+", and "+this.mode+" with new power "+pwr);
+		}
+	}
+}
