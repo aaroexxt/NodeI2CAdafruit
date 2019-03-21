4724b89ab8d714585347af5a4c1636c8c4ecda1d
aaroexxt <aaron.becker@gmail.com>
2019-03-20 22:54:14 -0700

    real fix

--- Si4713/scanner.js
+++ Si4713/scanner.js
@@ -5,37 +5,37 @@ const EBUSY = 16; /* Device or resource busy */
 
 class scanner {
     constructor(first, last) {
-      console.log('     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f');
+      process.stdout.write('     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f');
 
       for (let addr = 0; addr <= 127; addr += 1) {
         if (addr % 16 === 0) {
-          console.log((addr === 0 ? '0' : ''));
-          console.log(addr.toString(16) + ':');
+          process.stdout.write('\n' + (addr === 0 ? '0' : ''));
+          process.stdout.write(addr.toString(16) + ':');
         }
 
         if (addr < first || addr > last) {
-          console.log('   ');
+          process.stdout.write('   ');
         } else {
           try {
             i2c1.writeQuickSync(addr, 0);
-            console.log(' ' + addr.toString(16)); // device found, print addr
+            process.stdout.write(' ' + addr.toString(16)); // device found, print addr
           } catch (e) {
             if (e.errno === EBUSY) {
-              console.log(' UU');
+              process.stdout.write(' UU');
             } else {
-              console.log(' --');
+              process.stdout.write(' --');
             }
           }
         }
       }
 
-      console.log('\n');
+      process.stdout.write('\n');
     }
 }
 
 
 if (!i2c1.i2cFuncsSync().smbusQuick) {
-  console.log('Error: Can\'t use SMBus Quick Write command on this bus#');
+  process.stdout.write('Error: Can\'t use SMBus Quick Write command on this bus#');
 } else {
   let scan = new scanner(0x3, 0x77);
   i2c1.closeSync(); //close bus
