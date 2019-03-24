6e681c99c1755d0fcef311e881270881c6f7810c
aaroexxt <aaron.becker@gmail.com>
2019-03-23 20:54:15 -0700

    can't fiure this out

--- Si4713/controller.js
+++ Si4713/controller.js
@@ -129,13 +129,10 @@ class Si4713Driver extends LibCommon.device {
 	}
 
 	sendCommand(arrayBuffer = -1) {
-
-		var i2cBuffer = Buffer.from(arrayBuffer); //create real buffer
+		if (typeof buffer != "number") {
+			var i2cBuffer = Buffer.from(arrayBuffer); //create real buffer
 			iI.i2cWriteSync(this.i2caddr, i2cBuffer.length, i2cBuffer); //writes buffer to i2c addr
-
-
-		/*if (typeof buffer != "number") {
-			
+			setTimeout(()=>{})
 
 			if (debugMode) {
 				for (const b of i2cBuffer) {
@@ -146,7 +143,7 @@ class Si4713Driver extends LibCommon.device {
 			return i2cBuffer;
 		} else {
 			console.error("Buffer is a number");
-		}*/
+		}
 	}
 	getRev() {
 		return new Promise( (resolve, reject) => {
